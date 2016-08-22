26eb73ae7a1016fb9973edb5ef66a41d2482e335
CarlesCG <carles.campos@gmail.com>
2016-08-22 19:50:48 +0200

    - Fix layout size plot Weibull - Add 'Technicall info box'

--- R/module_generator.R
+++ R/module_generator.R
@@ -5,10 +5,12 @@ generator_UI <- function(id){
       mainPanel(
         box(title = "Weibull plot generator", width = 12,# height = 820, 
             status="info", solidHeader = T, collapsible=T,  
-            plotOutput( ns("logplot") ) 
-        ) 
+            plotOutput( ns("logplot") ) ), 
+        box(title = "Technicall info", 
+            status="info", solidHeader = T, collapsible=T,  
+            dataTableOutput(ns("legendPlot")  ) )
         
-      ), 
+        ), 
       sidebarPanel(
         # width = 2, 
         tabName="Variables", 
@@ -40,11 +42,11 @@ generator_Server <- function(input, output, session){
   # 0. Censored and Uncensored
   # Number of failures < Num. points xa Update slider Input with correction
   observe({
-     if(input$nFailures > input$npoints){
-       updateSliderInput(session, "nFailures", value = input$npoints)
-     }
-   })
-
+    if(input$nFailures > input$npoints){
+      updateSliderInput(session, "nFailures", value = input$npoints)
+    }
+  })
+  
   output$conditional_confinter <- renderUI({
     ns <- session$ns
     if(input$confinter == T){
@@ -53,53 +55,68 @@ generator_Server <- function(input, output, session){
     }
   })
   
-
-    ## 1. Plots --------------------------------------------
-    ## 1.1 Weibull Plot
-    # How the data will be generated
-    data.generator <- reactive({ 
-      set.seed(1234)
-      # Time & Event data 
-      df <- data.frame(time = rweibull(n = input$npoints, shape = input$shape, scale = input$scale) )
-      df$event <- c( rep(1, input$nFailures), rep(0, input$npoints - input$nFailures) )
-      
-      data.frame(time=df$time, event=df$event)
-      
-    })
-    output$logplot <- renderPlot({
-      # Aesthetics
-      title <- ""
-      subtitle <- ""
-      # Data greneration 
-      df <- data.generator()
-      
-      # Fit abrem 
-      dfa <- Abrem(df, col="black", pch=2)
-      dfa <- abrem.fit(dfa, col="blue")
-      
-      # Adding confident intervals
-      if(input$confinter == T){
-        subtitle <- paste0("Likelihood Ratio confidence level ",
-                           input$conflevel_generator*100," %", "(the red line)")
-
-        dfa <- abrem.conf(dfa, method.conf.blives="lrb", cl=input$conflevel_generator,
-                          unrel.n=25,
-                          lty=1, lwd=3, col="red")
-      }
-      
-      
-      # Plot
-      plot(dfa, main=title, sub=subtitle, is.plot.legend=T,
-           xlab="Time to Failure", 
-           ylab="Occurrence CDF %")
-    } ) 
+  
+  ## 1. Plots --------------------------------------------
+  ## 1.1 Weibull Plot
+  # How the data will be generated
+  data.generator <- reactive({ 
+    set.seed(1234)
+    # Time & Event data 
+    df <- data.frame(time = rweibull(n = input$npoints, shape = input$shape, scale = input$scale) )
+    df$event <- c( rep(1, input$nFailures), rep(0, input$npoints - input$nFailures) )
     
-    # output$download_weibullGenerator <- downloadHandler(
-    #   filename = "Plot_Weibull_generated.pdf",
-    #   content = function(file) {
-    #     observe(data.toWeibull())
-    #     dev.off()
-    #   }
-    # )
-
+    data.frame(time=df$time, event=df$event)
+    
+  })
+  weibull.generator <- reactive({
+    # Data greneration 
+    df <- data.generator()
+    
+    # Fit abrem 
+    dfa <- Abrem(df, col="black", pch=2)
+    dfa <- abrem.fit(dfa, col="blue")
+    
+    if(input$confinter == T){
+      dfa <- abrem.conf(dfa, method.conf.blives="lrb", cl=input$conflevel_generator,
+                        unrel.n=25,
+                        lty=1, lwd=3, col="red")
+    }
+    return(dfa)
+  })
+  
+  output$logplot <- renderPlot({
+    # Aesthetics: subtitle will be modify when the IF stament is tru
+    title <- ""
+    subtitle <- ""
+    
+    
+    # Adding confident intervals
+    if(input$confinter == T){
+      subtitle <- paste0("Likelihood Ratio confidence level ",
+                         input$conflevel_generator*100," %", "(the red line)")
+    }
+    
+    
+    # Plot
+    plot(weibull.generator(), 
+         main=title, sub=subtitle, is.plot.legend=T,
+         xlab="Time to Failure", 
+         ylab="Occurrence CDF %")
+  })
+  output$legendPlot <- renderDataTable({
+    source("R/foo_abremLegend.R")
+    WeibullLegend(weibull.generator())
+  }, options= list(
+    paging = F, 
+    searching = FALSE
+  ))
+  
+  # output$download_weibullGenerator <- downloadHandler(
+  #   filename = "Plot_Weibull_generated.pdf",
+  #   content = function(file) {
+  #     observe(data.toWeibull())
+  #     dev.off()
+  #   }
+  # )
+  
 }
