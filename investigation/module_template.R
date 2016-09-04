33b3daca5d38fc2c50e717d5fa08b34284c5f83d
CarlesCG <carles.campos@gmail.com>
2016-09-04 00:21:10 +0000

    MMoved files

---
+++ investigation/module_template.R
@@ -0,0 +1,21 @@
+NAME_UI <- function(id){
+  ns <- NS(id)
+  tagList(
+    sidebarLayout(
+      mainPanel(
+        # Stuff in the main panel
+      ),
+      sidebarPanel(
+        # Stuff in the side bar
+     
+      )
+    )
+  )
+}
+
+
+NAME_server <- function(input, output, session){
+  
+  # Calculations with output$whatever <- 
+ 
+}
