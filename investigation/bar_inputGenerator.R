22187d68f8995b062fe6fd6263e107c5c7b0b503
CarlesCG <carles.campos@gmail.com>
2016-09-16 15:25:36 +0200

    Add more inputs$()

--- investigation/bar_inputGenerator.R
+++ investigation/bar_inputGenerator.R
@@ -1,10 +1,15 @@
-library(abrem)
+# library(abrem)
 input <- c()
 input$npoints <- 10
 input$shape <- 1
 input$scale <- 750
 input$nFailures <- 26
 input$npoints <- 60
+input$obs <- 10
+input$beta <- 2
+input$dem_eta <- 500
+input$dem_eta <- .85
+
 input$conflevel_generator <- .8
 
 
@@ -15,6 +20,7 @@ df$event <- c( rep(1, input$nFailures), rep(0, input$npoints - input$nFailures) 
 df <- data.frame(time=df$time, event=df$event)
 dfa <- Abrem(df, col="black", pch=2)
 dfa <- abrem.fit(dfa, col="blue")
+plot( dfa )
 
 
 # df CONF_INTERVALS
