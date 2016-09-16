ae81bb83c833453904fab6c5e26a948db819f044
CarlesCG <carles.campos@gmail.com>
2016-09-16 15:23:59 +0200

    Add purr flatten to read the data

--- R/foo_abremLegend.R
+++ R/foo_abremLegend.R
@@ -1,8 +1,20 @@
 # Format Weibull plot legend into a table
-WeibullLegend <- function(r){
-  x <- r
+WeibullLegend <- function(abrem.object){
   
-  measure <- c(
+  x <- purrr::flatten(abrem.object)
+  variables <- names(x)
+  
+  df <- tibble(stat=x, names=variables)
+  t(df)
+  df
+  
+  # Make new columns with pretty description for plots
+  
+  
+  ####
+  ####
+  
+  description <- c(
     "Number of points", 
     "Failures", 
     "Censored")
@@ -14,7 +26,7 @@ WeibullLegend <- function(r){
   
   # results <- data.frame(measure, values)
   
-  results <- data.frame( measure, values)
+  results <- data.frame( variables , description, values)
   names(results) <- c("Measure", "Value")
   
   return(results)
@@ -22,31 +34,45 @@ WeibullLegend <- function(r){
   
 }
 
+flat_abrem <- function(x){
+  purrr::flatten(x)
+}
 
-investigate <- function(){
+investigate_notRun <- function(){
 # Extract parameter of the plot?? 
+load("./data/abrem_structure.RDat")
 dplyr::bind_rows(x)
 data.table::rbindlist(x)
 
+t <- purrr::flatten(x)
 
 # 0 level list 
 x$data
 x$n
 x$fail
 x$cens
+x$options # list
+x$fit # list
 
 # 1st level
 options <- x$options
 names(options)
 
+t <- x$options
+
 fit <- x$fit[[1]]
 fit$options
-
+fit$n
+fit$fail
+fit$cens
+fit$data
+fit$lm  
 fit$beta
 fit$eta
 fit$gof
 names(fit)
 
+fit$conf
 # Si hay confident intervals
 conf <- fit$conf$blives[[1]]
 conf$type
@@ -57,5 +83,5 @@ conf$MLEXContour
 conf$bounds
 conf$options
 
-}
 
+}
