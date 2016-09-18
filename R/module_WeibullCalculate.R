4d464db5e9385c69fcd28c6a36ceceb5cf66e0bc
CarlesCG <carles.campos@gmail.com>
2016-09-18 15:39:52 +0200

    Add Event density plot

--- R/module_WeibullCalculate.R
+++ R/module_WeibullCalculate.R
@@ -8,7 +8,10 @@ weibullCalculate_UI <- function(id){
             plotOutput( ns("abremplot") ) ), 
         box(title = "Weibull summary", width = 12,# height = 820, 
             status="info", solidHeader = T, collapsible=T,  
-            verbatimTextOutput( ns("test") ) )
+            verbatimTextOutput( ns("test") ) ), 
+        box(title = "Events Density ", width = 12,# height = 820, 
+            status="info", solidHeader = T, collapsible=T,  
+            plotOutput( ns("ggdensity") ) )
       ), 
       sidebarPanel(
         sliderInput( inputId = ns( "confcalc") , animate = TRUE,
@@ -31,14 +34,37 @@ weibullCalculate_UI <- function(id){
 
 
 weibullCalculate_server <- function(input, output, session, data){
+  source("./R/foo_make_abrem_plot.R")
+  
+  ## To test the plots
+  # data <- reactive({
+    read.csv("./data/Weibull_template.csv")
+  })
   
   output$test <- renderPrint( summary(data()) )
   
-  source("./R/foo_make_abrem_plot.R")
+  
   output$abremplot <- renderPlot({
-    dummy2 <- read.csv("./data/Weibull_template.csv")
-    make_abrem_plot(df = dummy, input, title = "This is my plot")
+    make_abrem_plot(df = data(), input, title = "This is my plot")
   })
+  
+  output$ggdensity <- renderPlot({
+    # TO IMPROVE:
+    # Does not generalize to more than One failure mode!
+   ggplot(data(), aes(x= time, fill=as.factor(event))) + 
+      geom_density(alpha=.2) +
+      scale_fill_manual(name=" ", 
+                        values=c("green","red"), 
+                        labels=c("Survived", "Failed")) +
+      geom_vline(data= data() %>% 
+                   filter(event ==1) %>% 
+                   summarise( stat = median(time, na.rm = T) ), 
+                 aes(xintercept=stat),
+                 linetype="dashed", size=1) +
+      theme_bw() +
+      ggtitle("Where did the event take place?")
+  })
+  
   
   # df <- callModule(uploadData_server, "page_uploadData2")
   # output$table <- renderDataTable({ df() })
