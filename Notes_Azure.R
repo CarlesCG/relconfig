6dc9ce0f54c7192c01d372e3bbde920725d2d51a
CarlesCG <carles.campos@gmail.com>
2016-11-07 16:28:12 +0000

    Add files notes Azure

---
+++ Notes_Azure.R
@@ -0,0 +1,3 @@
+install.packages("dplyr", repos = "http://cran.us.r-project.org")
+devtools::install_github("rstudio/rmarkdown")
+install.packages("rprojroot", repos = "http://cran.us.r-project.org")
