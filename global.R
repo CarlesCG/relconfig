c33d8f32354b7536938f9095bce41dcd5bfbd77e
CarlesCG <carles.campos@gmail.com>
2016-12-01 21:06:14 +0100

    Include the CSS. Options to optimize the aesthetics

--- global.R
+++ global.R
@@ -1,1 +1,12 @@
-# library(abrem)
+
+# Friss dashboard header
+RdynamicsHeader <- function(){
+  tags$head(
+  # tags$link(rel = "stylesheet", type = "text/css", href = "app.css"),
+  # tags$img(src="friss_small1.svg" , id = "FrissLogo"),
+  # tags$img(src="friss_subtext.svg", id = "FrissLogoText"),
+  # singleton(includeScript("www/d3.js")),
+  # singleton(includeScript("www/underscore.js")),
+  # singleton(includeScript("www/jquery-ui.js")),
+  singleton(includeCSS("./www/app.css"))
+)}
