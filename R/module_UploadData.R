eff58e2fa7f2659f2caeee26b64f231391bfbe8f
CarlesCG <carles.campos@gmail.com>
2016-12-02 18:26:34 +0100

    Add module code intro.js

--- R/module_UploadData.R
+++ R/module_UploadData.R
@@ -1,31 +1,52 @@
+library(rintrojs)
+
 uploadData_UI <- function(id){
   ns <- NS(id)
   tagList(
+    # Tutorial intro.js
+    introjsUI(),
+    
+    # Panel with the data uploaded
     sidebarLayout(
       mainPanel(
         box(title = "Data Uploaded", width = 12,# height = 820, 
             status="info", solidHeader = T, collapsible=T,  
             dataTableOutput(ns("test") ) )
       ),
+      
+      # Options to upload the data
       sidebarPanel(
+        id=ns("sidebar"), 
+        
         # Stuff in the side bar
         # Calling the Upload file Module
         fileInput( ns('file'), 'Choose CSV File',
                    accept=c('text/csv',
                             'text/comma-separated-values,text/plain',
-                            '.csv')),
+                            '.csv')) %>% 
+          div(id = ns("tut_choosefile")),
         
         tags$hr(),
-        checkboxInput( ns('header'), 'Header', TRUE),
+        checkboxInput( ns('header'), 'Header', TRUE) %>% 
+          div(id = ns("tut_header")),
+        
         radioButtons( ns('sep'), 'Separator',
                       c(Comma=',', Semicolon=';', Tab='\t'),
-                      ','),
+                      ',') %>% 
+          div(id = ns("tut_separator")),
         radioButtons(ns( 'quote' ), 'Quote',
                      c(None='', 'Double Quote'='"', 'Single Quote'="'"),
-                     '"'),
+                     '"') %>% 
+          div(id = ns("tut_quote")),
+        
         ## Esto moverlo a otr side bar panel
         h4("Need a template?"),
-        downloadButton(ns('downloadData'), 'Download')
+        downloadButton(ns('downloadData'), 'Download'), 
+        
+        # Help button
+        br(),
+        br(),
+        actionButton(ns("help"), "Guide")
       )
     )
   )
@@ -71,6 +92,18 @@ uploadData_server <- function(input, output, session){
     },
     contentType = "text/csv"
   )
+  
+  ## 2. The help function
+  steps <- readxl::read_excel("./text/tutorials/Tutoriales_introJS.xls", sheet = "page_uploadData")
+  
+  # start introjs when button is pressed with custom options and events
+  observeEvent(input$help,{
+    introjs(session, options = list(steps= steps,
+                                    "nextLabel"="Next",
+                                    "prevLabel"="Previous",
+                                    "skipLabel"="End"))
+  })
+  
   
   return(dataframe)
   
